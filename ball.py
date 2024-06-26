7d49d5af3642948a82b3b718db47128803ba987c
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2024-06-26 09:01:25 +0000

    correct a few bugs + add in time module

--- ball.py
+++ ball.py
@@ -1,17 +1,13 @@
 from turtle import Turtle
-import random
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.penup()
-        self.shapesize(stretch_len=10, stretch_wid=10)
         self.color("white")
-        self.speed("fastest")
-        self.refresh()
+        self.penup()
 
-    def refresh(self):
-        random_x = random.randint(-350, 350)
-        random_y = random.randint(-350, 350)
-        self.goto(random_x, random_y)
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
